9edd320f1de64715fc44aa65458c32975190a17b
ferchirinoboss <chirinoboss@gmail.com>
2024-06-25 10:50:19 -0600

    skeleton nav_agent
    remade the pathfinding logic this time using the navigatoragent2d. Works fine!!!! oooooooooooooooooooooo

--- scenes/enemies/skeleton/states/SkeletonWalk.gd
+++ scenes/enemies/skeleton/states/SkeletonWalk.gd
@@ -2,13 +2,27 @@ extends State
 class_name SkeletonWalk
 
 @onready var parent = $"../.."
+@onready var nav_agent = $"../../NavigationAgent2D"
 
 func enter():
-	pass 
+	$"../../NavigationAgent2D/NavigationTimer".start()
 	
 func Update(_delta):
 	pass
 	
-func Physics_Update(delta):
+func Physics_Update(_delta):
+	var direction = parent.to_local(nav_agent.get_next_path_position()).normalized()
+	
 	$"../../AnimationPlayer".play("walk")
-	parent.position  += (parent.player.position - parent.position) / parent.speed 
+	parent.velocity = direction * parent.speed
+	
+	if direction.x < 0:
+		$"../../Sprite2D".flip_h = true
+	else: 
+		$"../../Sprite2D".flip_h = false 
+		
+func make_path():
+	nav_agent.target_position = parent.player.global_position
+
+func _on_navigation_timer_timeout():
+	make_path()
